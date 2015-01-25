14fcbe774ebe49ff557516af0964a15407cfeb3d
TJ Stankus <tjstankus@gmail.com>
2015-01-25 00:13:09 -0500

    BowlingGame#score method passes initial test
    I made a mistake in the spec that needed to be corrected - I forgot the
    it block. Starting a new branch in order to get a clean commit history.

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -3,9 +3,11 @@ require 'bowling_game'
 RSpec.describe BowlingGame do
 
   describe 'gutter game' do
-    game = BowlingGame.new
-    20.times { game.roll(0) }
-    expect(game.score).to eq(0)
+    it 'scores 0' do
+      game = BowlingGame.new
+      20.times { game.roll(0) }
+      expect(game.score).to eq(0)
+    end
   end
 
 end
